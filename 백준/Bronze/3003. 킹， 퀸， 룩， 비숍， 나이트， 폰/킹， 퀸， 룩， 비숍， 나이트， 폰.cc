539fa20763ed26817415b1da655db31bd4408018
HEOJUNFO <100739943+HEOJUNFO@users.noreply.github.com>
2024-12-13 15:03:53 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int K, Q, R, B, N, P;
+    cin >> K >> Q >> R >> B >> N >> P;
+    
+    cout << (1 - K) << " "
+         << (1 - Q) << " "
+         << (2 - R) << " "
+         << (2 - B) << " "
+         << (2 - N) << " "
+         << (8 - P) << "\n";
+
+    return 0;
+}
